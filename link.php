e6fea2b84f16560963131df875e7b8eed40ffa6a
Raquel Brombilla <raquelbrombilla@hotmail.com>
2021-01-20 16:25:48 -0300

    links

---
+++ link.php
@@ -0,0 +1,15 @@
+    
+    <!-- Link bootstrap-->
+    <link rel='stylesheet' type='text/css' href='bootstrap/css/bootstrap.min.css'>
+    <!-- Link js-->
+    <script type='text/javascript' src='bootstrap/js/bootstrap.min.js'></script> 
+    <!-- Link CSS principal-->
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    <!-- Link Font Awesome -->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Link fonte menu -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	<!-- Link Fonte menu-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Link CSS menu -->
+    <link rel="stylesheet" href="menu/css/style.css">
---
+++ link_fim.php
@@ -0,0 +1,9 @@
+    
+        </div>  <!-- Fecha div wrapper menu-->
+        </div>  <!-- Fecha div content menu -->
+        
+ 
+    <script src="menu/js/jquery.min.js"></script>
+    <script src="menu/js/popper.js"></script>
+    <script src="menu/js/bootstrap.min.js"></script>
+    <script src="menu/js/main.js"></script>
